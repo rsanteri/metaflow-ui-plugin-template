2b338129b59e0ac98af144f8529bbd840b314d6a
Santeri Rönkkö <santeri.ronkko@codemate.com>
2021-07-12 10:31:25 +0300

    docs: Document plugin functions better

--- MetaflowPluginAPI.js
+++ MetaflowPluginAPI.js
@@ -35,9 +35,10 @@ function messageHandler(event) {
 }
 
 const Metaflow = {
-  //
-  // Update height of plugin to parent application. Useful if we want whole plugin to be visible
-  //
+  /**
+   *  Update height of plugin to parent application. Useful if we want whole plugin to be visible
+   * @param {number} fixedHeight Optional fixed height in pixels for plugin. If not given, we try to calculate plugin height automatically.
+   */
   setHeight(fixedHeight) {
     if (fixedHeight) {
       window.parent.postMessage({ name: window.name, type: 'PluginHeightCheck', height: fixedHeight }, '*');
@@ -47,38 +48,46 @@ const Metaflow = {
       window.parent.postMessage({ name: window.name, type: 'PluginHeightCheck', height: height }, '*');
     }
   },
-  //
-  // Register application
-  //
+  /**
+   * Register application to be rendered in app.
+   * @param {("headless"|"run-header"|"task-details")} slot 
+   * @param {(manifest: PluginManifest) => void} onReady 
+   */
   register(slot, onReady) {
     onReadyFn = onReady;
     window.parent.postMessage({ name: window.name, type: 'PluginRegisterEvent', slot: slot, version: VERSION_INFO }, '*')
     window.addEventListener('message', messageHandler);
   },
-  //
-  // Subscribe to data resources
-  //
+  /**
+   * Subscribe to data 
+   * @param {string[]} paths 
+   * @param {(event: { path: string, data: * }) => void} fn 
+   */
   subscribe(paths, fn) {
     Listeners.push(fn);
     window.parent.postMessage({ name: window.name, type: 'PluginSubscribeToData', paths: paths }, '*')
   },
-  //
-  // Subscribe to events
-  //
+  /**
+   * Subsribe to events 
+   * @param {string[]} events List of event name to subscribe to
+   * @param {(event: { type: string, data: * }) => void} fn Callback to trigger in case of event
+   */
   on(events, fn) {
     EventListeners.push(fn);
     window.parent.postMessage({ name: window.name, type: 'PluginSubscribeToEvent', events: events }, '*')
   },
-  //
-  // Call event
-  //
+  /**
+   * Call event with any name and payload. Other plugins or systems in app might subscribe to these events.
+   * @param {string} event
+   * @param {*} data 
+   */
   call(event, data) {
     window.parent.postMessage({ name: window.name, type: 'PluginCallEvent', event: event, data: data }, '*')
   },
-  //
-  // Send notification
-  // @param {string | {type: string, message: string}} message
-  //
+  /**
+   * Send notification on main application
+   * @param {string | {type: string, message: string}} message 
+   */
   sendNotification(message) {
     window.parent.postMessage({ name: window.name, type: 'PluginCallEvent', event: 'SEND_NOTIFICATION', data: message }, '*')
   },
